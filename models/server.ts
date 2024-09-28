c2fa16458521e73c35a6ab3ad55712f1033733fd
ezeabregu <ezeabregu.ea@gmail.com>
2024-09-28 20:04:05 -0300

    some changes

--- models/server.ts
+++ models/server.ts
@@ -1,14 +1,17 @@
 import express, { Express } from "express";
 import { dbConnection } from "../database/config";
 import cors from "cors";
+import authRoutes from "../routes/auth";
 
 export class Server {
   app: Express;
   port: string | number | undefined;
+  authPath: string;
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.authPath = "/auth";
     this.conectarDB();
     this.middlewares();
     this.routes();
@@ -20,7 +23,9 @@ export class Server {
     this.app.use(express.json());
     this.app.use(cors());
   }
-  routes(): void {}
+  routes(): void {
+    this.app.use(this.authPath, authRoutes);
+  }
   listen(): void {
     this.app.listen(this.port, () => {
       console.log(`Corriendo en puerto ${this.port}`);
