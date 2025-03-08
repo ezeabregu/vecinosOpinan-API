85e86843d1ddb821f9f317609bad63d62e83e353
ezeabregu <ezeabregu.ea@gmail.com>
2025-03-08 15:55:28 -0300

    port changes

--- models/server.ts
+++ models/server.ts
@@ -10,7 +10,7 @@ export class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT ?? 8080;
     this.authPath = "/auth";
     this.conectarDB();
     this.middlewares();
